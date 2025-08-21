96e1b38a0bd62fd21cc02c0ae031ad0683763bb3
unknown <ladrera21@gmail.com>
2025-08-22 01:41:56 +0800

    adding login function dtr onsite

--- src/pages/reusable-pages/dtr-onsite-login-page/LoginForOnsite.jsx
+++ src/pages/reusable-pages/dtr-onsite-login-page/LoginForOnsite.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './LoginForOnsite.css'
+import { loginQiosk } from '../../../store/features/authEmployee';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from "react-router-dom";
 
 const LoginForOnsite = () => {
+
+  const dispatch = useDispatch();
+
+  const { kioskData, loading, isSuccess } = useSelector(state => state.auth);
+
+  const [pinCode, setPinCode] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+
+    if(pinCode !== '') {
+      await dispatch(loginQiosk(pinCode));
+    } else {
+      alert("Enter PiN code");
+    }
+
+  }
+
+  useEffect(() => {
+    if(isSuccess) {
+      navigate("/dtr-onsite-access");
+    }
+  }, [kioskData, isSuccess])
+  
+
   return (
     <div className="login-container d-flex justify-content-center align-items-center vh-100 bg-danger">
       <div className="login-box text-center p-4 rounded shadow">
-        <h2 className="text-white mb-4">Welcome to the Kiosk</h2>
-        <form>
+        <h2 className="text-white mb-4">Welcome Back</h2>
           <div className="mb-3">
             <label htmlFor="code" className="form-label text-white fs-5">
               Enter Access Code
@@ -16,12 +44,12 @@ const LoginForOnsite = () => {
               className="form-control form-control-lg text-center"
               id="code"
               placeholder="••••••"
+              onChange={(e) => {setPinCode(e.target.value)}}
             />
           </div>
-          <button type="submit" className="btn btn-light btn-lg w-100">
-            Login
+          <button className="btn btn-light btn-lg w-100" onClick={handleLogin} disabled={loading}>
+            {loading ? "Loading..." : "Login"}
           </button>
-        </form>
       </div>
     </div>
   );
