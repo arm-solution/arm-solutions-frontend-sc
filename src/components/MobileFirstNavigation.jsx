8641c393f9ce386c23c41e5cdd85632697765412
jaredladrera <jaredladrera21@gmail.com>
2026-02-04 15:49:13 +0800

    adding design for notification

--- src/components/MobileFirstNavigation.jsx
+++ src/components/MobileFirstNavigation.jsx
@@ -10,7 +10,7 @@ const MobileFirstNavigation = ({ handleToggle }) => {
                   <i className="lni lni-grid-alt text-white font-weight-bold"></i>
           </button>
           <div className="flex-grow-1 text-center">
-                <p className="text-white m-0 mx-3 mr-5">Dashboardsss</p>
+                <p className="text-white m-0 mx-3 mr-5">Dashboard</p>
             </div>
           </div>
         </nav>    
